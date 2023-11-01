67e77c982ffa7bfdfb92fb5276c785b572c13fcc
imadelbouazzaoui <imad.elbouazzaoui.ee@gmail.com>
2023-11-01 15:33:39 +0100

    Part 2 completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+
+var accountBalance=(0..1000).random()
+
 fun main() {
     var accountType=""
     var userChoice=0
@@ -19,5 +22,60 @@ fun main() {
             3 -> accountType = "checking"
         }
         println("You have created a $accountType account.")
+    }
+    println("Account Balance: $accountBalance")
+    val money=(0..1000).random()
+    println("deposited/withdrawn Balance: $money")
+    var output=0
+    output = withdraw(money)
+    println("output: $output")
+    output = debitWithdraw(money)
+    println("output: $output")
+    output = deposit(money)
+    println("output: $output")
+    output = creditDeposit(money)
+    println("output: $output")
+
+}
+
+fun withdraw(amount: Int):Int{
+    accountBalance-=amount
+    println("Amount of money withdrawn: $amount, account’s updated balance: $accountBalance")
+    return amount
+}
+
+fun debitWithdraw(amount: Int):Int{
+
+    if(accountBalance==0){
+        println("Can't withdraw, no money on this account!")
+        return accountBalance
+    }else if(amount > accountBalance){
+        println("Not enough money on this account! The checking balance is ${accountBalance} dollars.")
+        return 0
+    }else{
+        return withdraw(amount)
+    }
+}
+
+fun deposit(amount: Int): Int{
+    accountBalance+=amount
+    println("Amount of money deposited: $amount, account’s updated balance: $accountBalance")
+    return amount
+}
+
+fun creditDeposit(amount: Int):Int{
+    if(accountBalance==0){
+        println("You don't need to deposit anything in order to pay off the account since it has already been paid off.")
+        return accountBalance
+    }else if((accountBalance + amount)>0){
+        println("Deposit failed, you tried to pay off an amount greater than the credit balance. The checking " +
+                "balance is $accountBalance dollars")
+        return 0
+    }else if(amount==-accountBalance){
+        accountBalance = 0
+        println("You have paid off this account!")
+        return amount
+    }else{
+        return deposit(amount)
     }
 }
